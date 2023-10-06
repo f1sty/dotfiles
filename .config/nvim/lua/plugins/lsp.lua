0a7ad5238ca532ba0992c0a9ebca420363550d5d
Yurii Skrynnykov <truef1s7@gmail.com>
2023-10-06 17:14:49 +0300

    Update

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,7 +2,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
     vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, { buffer = args.buf })
-    vim.keymap.set('n', '<leader>lr', vim.lsp.buf.references, { buffer = args.buf })
+    vim.keymap.set('n', '<leader>lR', vim.lsp.buf.references, { buffer = args.buf })
     vim.keymap.set('n', '<leader>li', vim.lsp.buf.implementation, { buffer = args.buf })
     vim.keymap.set('n', '<leader>ld', vim.lsp.buf.definition, { buffer = args.buf })
     vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, { buffer = args.buf })
